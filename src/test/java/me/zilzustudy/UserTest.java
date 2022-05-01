36b02b7d48b8a8fb085925fb9e44c7c3de4149d4
zilzu <zilzu4165@naver.com>
2022-05-01 22:45:45 +0900

    Test refactoring

--- src/test/java/me/zilzustudy/UserTest.java
+++ src/test/java/me/zilzustudy/UserTest.java
@@ -4,7 +4,6 @@ import me.zilzustudy.user.UserConfiguration;
 import me.zilzustudy.user.UserServiceImpl;
 import me.zilzustudy.user.ZilzuUserServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,23 +12,42 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class UserTest {
 
-
     // https://gist.github.com/Toparvion/26e2baf3b3fb89fa2f957e78fde5ef53  참고하였음
     private final ApplicationContextRunner runner = new ApplicationContextRunner()
-            .withInitializer(new ConditionEvaluationReportLoggingListener())
             .withUserConfiguration(UserConfiguration.class);
-//            .withUserConfiguration("zilzuUserServiceImpl", "userServiceImpl");
 
-    // 문제있네이거
+
+    // given
+
+    // when
+
+    // then
+
     @Test
-    public void property() {
-        runner
-                .withPropertyValues("get.user.config=false")
+    void config가_false이면_ZilzuUserServiceImple을_가져온다() {
+        configPropertyValueIs(false)
                 .run(context -> assertAll(
-//                        () -> assertThat(context).hasSingleBean(UserServiceImpl.class)
-//                        () -> assertThat(context).hasSingleBean(ZilzuUserServiceImpl.class)
-//                        () -> assertThat(context).doesNotHaveBean(ZilzuUserServiceImpl.class)
-                        () -> assertThat(context).doesNotHaveBean("zilzuUserServiceImpl")
+                        () -> assertThat(context).hasSingleBean(ZilzuUserServiceImpl.class),
+                        () -> assertThat(context).doesNotHaveBean(UserServiceImpl.class)
                 ));
+    }
+
+    @Test
+    void config가_true이면_UserServiceImple을가져온다() {
+        configPropertyValueIs(true)
+                .run(context -> assertAll(
+                        () -> assertThat(context).hasSingleBean(UserServiceImpl.class),
+                        () -> assertThat(context).doesNotHaveBean(ZilzuUserServiceImpl.class)
+                ));
+    }
+
+    public ApplicationContextRunner configPropertyValueIs(boolean t) {
+        if (t) {
+            return runner
+                    .withPropertyValues("get.user.config=true");
+        } else {
+            return runner
+                    .withPropertyValues("get.user.config=false");
+        }
     }
 }
